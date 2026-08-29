07da06df5f88e8b6bbd10846d0e2bfb1a4791cd9
Fabrix Developer <admin@fabrix.sbs>
2026-08-29 14:08:01 +0530

    Add web app manifest.json and service worker sw.js for PWABuilder APK generation

---
+++ static/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'fabrix-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/style.css',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
